2288006de8ccc5a340b53e2b6f7c8da961019365
NIXKim <dbb04017@gmail.com>
2025-10-28 11:25:03 +0900

    6045 : [기초-산술연산] 정수 3개 입력받아 합과 평균 출력하기(설명)(py) 해결(Solve)

--- codeup.py
+++ codeup.py
@@ -1,12 +1,11 @@
-# 6043 : [기초-산술연산] 실수 2개 입력받아 나눈 결과 계산하기(py)
+# 6045 : [기초-산술연산] 정수 3개 입력받아 합과 평균 출력하기(설명)(py)
 
-a, b = input().split()
+a, b, c = input().split()
 a = int(a)
 b = int(b)
+c = int(c)
 
-print(a + b)
-print(a - b)
-print(a * b)
-print(a // b)
-print(a % b)
-print(format(a / b, ".2f"))
+d = a + b + c
+e = format(d/3, ".2f")
+
+print(d, e)
