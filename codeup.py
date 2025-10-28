3606c60d0cb06eca6aea5c6e7efd6e3256fb8b13
NIXKim <dbb04017@gmail.com>
2025-10-28 16:39:31 +0900

    6057 : [기초-논리연산] 참/거짓이 서로 같을 때에만 참 출력하기(설명)(py) 해결(Solve)

--- codeup.py
+++ codeup.py
@@ -1,6 +1,6 @@
-# 6056 : [기초-논리연산] 참/거짓이 서로 다를 때에만 참 출력하기(설명)(py)
+# 6057 : [기초-논리연산] 참/거짓이 서로 같을 때에만 참 출력하기(설명)(py)
 a, b = input().split()
-a = bool(int(a))
-b = bool(int(b))
+a = int(a)
+b = int(b)
 
-print((a and (not b)) or ((not a) and b))
+print((bool(not a)) and (bool(not b)) or (bool(a) and bool(b)))
