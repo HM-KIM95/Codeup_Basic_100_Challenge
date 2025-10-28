b0650858afe08f5f3b8185ce7f1e4f0affdb643c
NIXKim <dbb04017@gmail.com>
2025-10-28 11:19:44 +0900

    6044 : [기초-산술연산] 정수 2개 입력받아 자동 계산하기(py) 해결(Solve)

--- codeup.py
+++ codeup.py
@@ -1,6 +1,12 @@
 # 6043 : [기초-산술연산] 실수 2개 입력받아 나눈 결과 계산하기(py)
 
 a, b = input().split()
-s = float(a) / float(b)
+a = int(a)
+b = int(b)
 
-print(format(s, ".3f"))
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+print(format(a / b, ".2f"))
