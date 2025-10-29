f9a0d878d1dbe7058e350f90462c5dfb39afea37
NIXKim <dbb04017@gmail.com>
2025-10-29 17:57:47 +0900

    6064 : [기초-3항연산] 정수 3개 입력받아 가장 작은 값 출력하기(설명)(py) 해결(Solve)

--- codeup.py
+++ codeup.py
@@ -1,8 +1,9 @@
-# 6063 : [기초-3항연산] 정수 2개 입력받아 큰 값 출력하기(설명)(py)
+# 6064 : [기초-3항연산] 정수 3개 입력받아 가장 작은 값 출력하기(설명)(py)
 
-a, b = input().split()
+a, b, c = input().split()
 a = int(a)
 b = int(b)
-c = (a if (a>=b) else b)
+c = int(c)
+d = ((a if (a<=b) else b) if ((a if (a<=b) else b)<=c) else c)
 
-print(c)
+print(d)
