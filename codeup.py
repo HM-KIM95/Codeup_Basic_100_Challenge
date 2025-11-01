611112b94ccaba8c253451098db3a6fd397cdff8
NIXKim <dbb04017@gmail.com>
2025-11-01 23:34:57 +0900

    Q.6068 Solve

--- codeup.py
+++ codeup.py
@@ -1,16 +1,16 @@
-# 6067 : [기초-조건/선택실행구조] 정수 1개 입력받아 분류하기(설명)(py)
+# 6068 : [기초-조건/선택실행구조] 점수 입력받아 평가 출력하기(설명)(py)
 
 n = input()
 n = int(n)
 
-if n < 0:
-    if n % 2 == 0:
-        print('A')
-    else:
-        print("B")
-else:
-    if n % 2 == 0:
-        print("C")
-    else:
-        print("D")
+if n >= 90:
+    print('A')
     
+else:
+    if n >= 70:
+        print('B')
+    else:
+        if n >= 40:
+            print('C')
+        else:
+            print('D')
