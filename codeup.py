621901db6533bf2a7d4cc5a2b5bf39e9e07799c2
NIXKim <dbb04017@gmail.com>
2025-11-06 00:42:14 +0900

    Q.6072 Solved

--- codeup.py
+++ codeup.py
@@ -1,11 +1,5 @@
-# 6071 : [기초-반복실행구조] 0 입력될 때까지 무한 출력하기(설명)(py)
-
 n = int(input())
 
-# while: ~할 때 까지 ex) while n!=0 (n이 0이 될 때 까지)
-n = 1
-
 while n!=0 :
-    n = int(input())
-    if n!= 0:
-        print(n)
+    print(n)
+    n -= 1
