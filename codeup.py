98dd03cf6c4f81fffa86cf1d3b6b152395202b7c
NIXKim <dbb04017@gmail.com>
2025-11-17 00:39:05 +0900

    Q.6079 Solved

--- codeup.py
+++ codeup.py
@@ -1,7 +1,10 @@
 n = int(input())
-s = 0
-for i in range(1, n+1):
-    if i % 2 == 0:
-        s += i
-        
-print(s)
+
+i = 0
+sum = 0
+
+while n > sum:
+    i += 1
+    sum += i
+    
+print(i)
